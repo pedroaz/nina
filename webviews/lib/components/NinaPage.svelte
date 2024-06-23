2dc16c0afc2265a294614a746549109a7ff607f8
Pedro Azevedo <pedro.azvm@gmail.com>
2024-06-23 16:11:02 +0200

    adding nodes

--- webviews/lib/components/NinaPage.svelte
+++ webviews/lib/components/NinaPage.svelte
@@ -1,37 +1,27 @@
 <script lang="ts">
-  import {
-    SvelteFlow,
-    Background,
-    Controls,
-    BackgroundVariant,
-  } from "@xyflow/svelte";
-
+  import { SvelteFlow, Background } from "@xyflow/svelte";
   import { nodes, edges } from "../stores/NinaStore";
-
   import "@xyflow/svelte/dist/style.css";
   import { NodeManager } from "../services/NodeManager";
+  import { onMount } from "svelte";
+  import { getFileName } from "../services/Utils";
 
-  // onMount(() => {
-  //   window.addEventListener("message", (event) => {
-  //     const message = event.data; // The json data that the extension sent
-  //     switch (message.command) {
-  //       case "addFile":
-  //         console.log(message.data);
-  //         break;
-  //     }
-  //   });
-  // });
+  onMount(() => {
+    window.addEventListener("message", (event) => {
+      const message = event.data; // The json data that the extension sent
+      switch (message.command) {
+        case "addFile":
+          var filename = getFileName(message.data.path);
+          NodeManager.createNode(filename);
+          break;
+      }
+    });
+  });
 </script>
 
 <main>
-  <button
-    on:click={() => {
-      console.log("clicked");
-      NodeManager.createNode("TEST");
-    }}>Add New Node</button
-  >
   <SvelteFlow {nodes} {edges} fitView>
-    <Background bgColor="rgba(126,159,219,0.5)" patternColor="white" />
+    <Background bgColor="rgba(126,159,219,0.5)" />
   </SvelteFlow>
 </main>
 
--- webviews/lib/stores/NinaStore.ts
+++ webviews/lib/stores/NinaStore.ts
@@ -1,16 +1,6 @@
+import type { Edge, Node } from "@xyflow/svelte";
 import { writable } from "svelte/store";
 
-export const nodes = writable([
-  {
-    id: "1", // required and needs to be a string
-    position: { x: 0, y: 0 }, // required
-    data: { label: "hey" }, // required
-  },
-  {
-    id: "2",
-    position: { x: 100, y: 100 },
-    data: { label: "world" },
-  },
-]);
+export const nodes = writable([] as Array<Node>);
 
-export const edges = writable([{ id: "1-2", source: "1", target: "2" }]);
+export const edges = writable([] as Array<Edge>);
