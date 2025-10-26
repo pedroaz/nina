c0ddd2c81cc81c4737b4fee39fe48c41734c69ad
Pedro Azevedo <pedro.azvm@gmail.com>
2025-10-26 22:03:30 +0100

    add api with shared code

---
+++ packages/shared/index.ts
@@ -0,0 +1,6 @@
+// Export all models
+export * from './models/lesson.js';
+
+// Add more exports here as you create more files in models/
+// export * from './models/user.js';
+// export * from './models/course.js';
