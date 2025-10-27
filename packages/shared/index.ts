c79e5f67b609dca730dbe4bd6d6fff1b87902e92
Pedro Azevedo <pedro.azvm@gmail.com>
2025-10-27 07:45:19 +0100

    add playright e2e

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,5 +1,6 @@
 // Export all models
 export * from './models/lesson.js';
+export * from './models/userProfile.js';
 
 // Add more exports here as you create more files in models/
 // export * from './models/user.js';
