ff302304a55e33c7dd55f1aad5a63548fac3f0b1
Pedro Azevedo <pedro.azvm@gmail.com>
2025-12-09 08:46:31 +0100

    fixing eslint

--- packages/web-app/eslint.config.mjs
+++ packages/web-app/eslint.config.mjs
@@ -10,7 +10,6 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
       "node_modules/**",
@@ -20,6 +19,7 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
